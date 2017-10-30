4cdf981e544705492cf57d9644c1b015ae4b9368
Geyang Xie <xiegeyang@gmail.com>
2017-10-29 23:36:09 -0500

    Init

---
+++ App.js
@@ -0,0 +1,51 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Splash from './Splash';
+import Login from './src/components/Login/Login';
+
+const Application = StackNavigator(
+  {Home: {screen: Login}}, 
+  {navigationOptions: {
+    header:false,
+  }}
+  );
+
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+        <Application/>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
